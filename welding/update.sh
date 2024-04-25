5942b6f026998889fa2e14e787765977a4901cea
pi <pi@gmail.com>
2024-04-25 16:14:09 +0330

    fix: repair udate script

--- welding/update.sh
+++ welding/update.sh
@@ -4,6 +4,6 @@ rm /usr/bin/welding
 cp /home/pi/ProdcutBinaries/welding/welding /usr/bin/
 killall welding
 chmod 777 /usr/bin/welding
-./usr/bin/welding &
+/usr/bin/welding &
 echo  `date`  >>  /home/pi/update.log
 
