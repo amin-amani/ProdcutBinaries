b3b60b1cf144725cf3e0b5735e2595ad8bf09172
pi <pi@gmail.com>
2024-04-25 15:48:09 +0330

    feat: restart app after update

--- welding/update.sh
+++ welding/update.sh
@@ -2,3 +2,8 @@
 apt -y install git
 rm /usr/bin/welding
 cp /home/pi/ProdcutBinaries/welding/welding /usr/bin/
+killall welding
+chmod 777 /usr/bin/welding
+./usr/bin/welding &
+echo  `date`  >>  /home/pi/update.log
+
