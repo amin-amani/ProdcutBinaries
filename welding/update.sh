2dc092301237187ca35065946ac249bbf6f00450
pi <pi@gmail.com>
2024-04-25 16:27:03 +0330

    update

--- welding/update.sh
+++ welding/update.sh
@@ -3,7 +3,9 @@ apt -y install git
 rm /usr/bin/welding
 cp /home/pi/ProdcutBinaries/welding/welding /usr/bin/
 killall welding
+
 chmod 777 /usr/bin/welding
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games
 /usr/bin/welding &
 echo  `date`  >>  /home/pi/update.log
 
